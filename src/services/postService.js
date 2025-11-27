2ab68048a5df38fd674c068bf22718aac4683629
Sree Teja <msreeteja24@gmail.com>
2025-11-27 15:07:23 +0530

    Creating a service layer with Axios

---
+++ src/services/postService.js
@@ -0,0 +1,13 @@
+//import axios
+import axios from "axios";
+
+//create a instance of Axios.
+//It will have baseURL - where BaseURL is always the URL where the API lives
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com/",
+});
+
+//Now we will the functions (for get, post, delete ..)
+const getPost = api.get("/posts");
+
+export { getPost };
