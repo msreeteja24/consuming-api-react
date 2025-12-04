5faa1c606f70b7d07f5cfa0b91468e96c2583f88
Sree Teja <msreeteja24@gmail.com>
2025-12-04 16:24:43 +0530

    Create post and post state after creating a new one

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,13 +1,46 @@
 import React, { useState } from "react";
+import { createPost } from "../services/postService";
 
-export default function CreatePost(posts, setPosts) {
+export default function CreatePost({ posts, setPosts }) {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
+  const addPost = () => {
+    createPost({ title, body })
+      .then((response) => {
+        setPosts([...posts, response.data]);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); //To prevent the reload of page when form is submitted
+
+    addPost();
+
+    setTitle("");
+    setBody("");
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h1> Create Post </h1>
-      <div></div>
-    </div>
+      <div>Title</div>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
+      <div>Body</div>
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      ></textarea>
+      <div>
+        <button type="submit">Add Post</button>
+      </div>
+    </form>
   );
 }
